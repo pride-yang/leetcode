04716e8ebad9d168c1347c6edbefe231d3ed577d
yangzh <yangzh@biyouxinli.com>
2020-12-01 11:15:44 +0800

    120

--- python/120.三角形最小路径和.py
+++ python/120.三角形最小路径和.py
@@ -12,14 +12,11 @@ from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [[0] * len(triangle+1)
-              for _ in range(len(triangle[0])+1)]
-        print(dp)
-        for i in range(len(triangle)-2,-1,-1):
-            for j in range(i+1):
-                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j]
-
-        return dp[0][0]
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j],
+                                      triangle[i + 1][j + 1])
+        return triangle[0][0]
 
 
 # @lc code=end
